93520eef6f87dc5d7e4542612a271450f9626c58
Антон Телышев <anton.telishev@yandex.ru>
2017-05-11 11:44:39 +0300

    Show exe location

---
+++ lab45/XmlParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XmlParser
+{
+    class Word {
+        public int Long;
+    }
+
+    class WordList
+    {
+        public Word[] Words;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String currentLocation = System.Reflection.Assembly.GetEntryAssembly().Location;
+            Console.WriteLine(currentLocation);
+        }
+    }
+}
